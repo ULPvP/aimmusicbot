94626dbce2e841a0e0ee718712bd27a53617f391
ULPvP <50048718+ULPvP@users.noreply.github.com>
2019-12-31 16:01:38 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,7 +63,7 @@ client.on('message', async message => {
 
         if (
       url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/))
-      )
+      
     ) {
       try {
         const playlist = await youtube.getPlaylist(url); // get playlist data 
